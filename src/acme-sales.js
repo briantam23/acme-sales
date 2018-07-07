1d15a715eefae3eb8aef99862caee6ea4fd245e2
briantam23 <briantam23@gmail.com>
2018-07-07 14:17:55 -0400

    finished

--- src/acme-sales.js
+++ src/acme-sales.js
@@ -7,31 +7,74 @@ const sales = [
     {
         customerId: 2,
         orderId: 2,
-        total: 33
+        total: 32
+    },
+    {
+        customerId: 2,
+        orderId: 3,
+        total: 1
+    },
+    {
+        customerId: 1,
+        orderId: 3,
+        total: 3
     }
 ];
 const customers = [
     {
-        id: 1,
+        id: 2,
         name: 'moe'
     },
     {
         id: 2,
         name: 'larry'
+    },
+    {
+        id: 3,
+        name: 'curly'
     }
 ];
 function generateCustomerSalesMap(salesData, customerData) {
-    /*return an object where the keys are the customer names
-    and the values are the total sales for that customer.
-    Ex: {
-        moe: 55;
-        larry: 33;
-        curly: 0;
-        }
-    */
-    
-    
+    let customerObj = {};
+    let customerNames = [];
+    let salesTotal = [];
+    let customerId = [];
+    let id = [];
+    let placeHolder = 0;
 
-    return {};
+    for(let j = 0; j < salesData.length; j++) {
+        if(customerId.indexOf(salesData[j].customerId) === -1) {  //distinct customerId
+          customerId.push(salesData[j].customerId);
+          salesTotal.push(salesData[j].total);
+        }
+        else { //same customerId
+          customerId.push(salesData[j].customerId);
+          salesTotal.push(salesData[j].total);
+          let placeHolder = salesData[j].customerId;
+          salesTotal[placeHolder - 1] += salesTotal.pop();   //add up different order totals for same customerId
+        }
+    };
+    for(let i = 0; i < customerData.length; i++) {
+        customerNames.push(customerData[i].name);
+        id.push(customerData[i].id);
+        
+    };
+    for(let h = 0; h < id.length; h++) {
+        if(customerId.indexOf(id[h]) !== -1) {  //check if customer has any sales
+          customerObj[customerNames[h]] = salesTotal[h]; 
+        }
+        else {
+          customerObj[customerNames[h]] = 0;
+        }
+    }
+    
+    console.log(customerId);
+    console.log(id);
+    console.log(customerNames);
+    console.log(salesTotal);
+    console.log(customerObj);
+    return customerObj;
 }
+generateCustomerSalesMap(sales, customers);
+
 module.exports = generateCustomerSalesMap;
--- test/acme-sales.spec.js
+++ test/acme-sales.spec.js
@@ -4,12 +4,14 @@ const generateCustomerSalesMap = require('../src/acme-sales')
 const sales = require('../src/acme-sales')
 const customers = require('../src/acme-sales')
 
+
+
 describe('acme-sales', () => {
     it('has the function generateCustomerSalesMap', () => {
         expect(generateCustomerSalesMap).to.be.ok;
     })
     it('returns an object', () => {
-        expect(typeof generateCustomerSalesMap()).to.eql('object');
+        expect(typeof generateCustomerSalesMap([],[])).to.eql('object');
     })
     it('returns the right results', () => {
         expect(generateCustomerSalesMap(sales, customers)).to.eql({
