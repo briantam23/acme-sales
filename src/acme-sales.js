b15305afe40062f46ef10becbbb896842ec6f992
briantam23 <briantam23@gmail.com>
2018-07-07 10:52:56 -0400

    first commit

---
+++ src/acme-sales.js
@@ -0,0 +1,37 @@
+const sales = [
+    {
+        customerId: 1,
+        orderId: 1,
+        total: 55
+    },
+    {
+        customerId: 2,
+        orderId: 2,
+        total: 33
+    }
+];
+const customers = [
+    {
+        id: 1,
+        name: 'moe'
+    },
+    {
+        id: 2,
+        name: 'larry'
+    }
+];
+function generateCustomerSalesMap(salesData, customerData) {
+    /*return an object where the keys are the customer names
+    and the values are the total sales for that customer.
+    Ex: {
+        moe: 55;
+        larry: 33;
+        curly: 0;
+        }
+    */
+    
+    
+
+    return {};
+}
+module.exports = generateCustomerSalesMap;
---
+++ test/acme-sales.spec.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+//const sinon = require('sinon');
+const generateCustomerSalesMap = require('../src/acme-sales')
+const sales = require('../src/acme-sales')
+const customers = require('../src/acme-sales')
+
+describe('acme-sales', () => {
+    it('has the function generateCustomerSalesMap', () => {
+        expect(generateCustomerSalesMap).to.be.ok;
+    })
+    it('returns an object', () => {
+        expect(typeof generateCustomerSalesMap()).to.eql('object');
+    })
+    it('returns the right results', () => {
+        expect(generateCustomerSalesMap(sales, customers)).to.eql({
+            moe: 55,
+            larry: 33,
+            curly: 0
+        });
+    })
+})
